c2a0a2b62c2e2aee4856033dbdec2d3ed36b3c0a
safiul <safiul.alam@tuni.fi>
2021-12-11 15:16:04 +0200

    asd

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-/*
-test('renders learn react link', () => {
+
+test('renders tasks', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Learn React/i);
+  const linkElement = screen.getByText(/TASKS/i);
   expect(linkElement).toBeInTheDocument();
 });
-*/
