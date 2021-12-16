7f05074bb30b06bfefb15a5a5ec4b01df9e2f1f7
safiul <safiul.alam@tuni.fi>
2021-12-16 07:24:29 +0200

    update tests url

--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -17,7 +17,7 @@ describe('app', () => {
 describe('GET /api/v1', () => {
   it('responds with a json message', (done) => {
     request(app)
-      .get('/api/v1')
+      .get('/v1')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, {
